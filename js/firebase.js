51e70488c3403ac9bb0bea409f1fcba92b2a692e
Yahiaahmedseid <yahiaahmedseid@gmail.com>
2026-07-19 03:14:59 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,9 +1,10 @@
-here// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+import {
+  getFirestore,
+  enableIndexedDbPersistence
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDfGLNhCwLr_gAxV9LRz0Ppm7Feb-iZYt4",
   authDomain: "studio-1737295542-906cd.firebaseapp.com",
@@ -14,5 +15,16 @@ const firebaseConfig = {
   appId: "1:986728437294:web:56e4bbd5ce2ff50448b1bf"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+// تفعيل التخزين المؤقت (Offline Persistence)
+enableIndexedDbPersistence(db)
+  .then(() => {
+    console.log("Offline persistence enabled");
+  })
+  .catch((err) => {
+    console.error("Persistence error:", err);
+  });
